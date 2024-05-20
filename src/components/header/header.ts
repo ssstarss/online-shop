38d00318788c8edbf1a9e68039dadad74e8d09d1
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2024-05-20 15:22:01 +0300

    feat: add burger menu and mobile navigation

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -9,7 +9,7 @@ const headerLinksList = createElement({ tag: 'ul', className: 'header__links-lis
 
 export const headerLinkHome = createElement({
   tag: 'li',
-  className: 'header__list-item home-link active',
+  className: 'header__list-item home-link active-header',
   textContent: 'Home',
 });
 
@@ -28,9 +28,9 @@ export const headerLinkBlogs = createElement({
 headerLinksList.append(headerLinkHome, headerLinkCatalog, headerLinkBlogs);
 
 const linkWrap = createElement({ tag: 'div', className: 'header__wrap' });
-const searchLink = createElement({ tag: 'a', className: 'header__search' });
+export const searchLink = createElement({ tag: 'a', className: 'header__search' });
 searchLink.innerHTML = search;
-const basketLink = createElement({ tag: 'a', className: 'header__basket' });
+export const basketLink = createElement({ tag: 'a', className: 'header__basket' });
 basketLink.innerHTML = basket;
 
 export const loginButton = createElement({
@@ -44,7 +44,63 @@ const loginText = createElement({
   textContent: 'Login',
 });
 loginButton.append(loginText);
+
+// burger
+export const burgerMenu = createElement({ tag: 'button', className: 'burger-menu' });
+const burgerMenuLine = createElement({ tag: 'span', className: 'burger-menu__line' });
+burgerMenu.append(
+  burgerMenuLine.cloneNode(),
+  burgerMenuLine.cloneNode(),
+  burgerMenuLine.cloneNode()
+);
+
+// Creating new elements for the mobile menu
+const mobileLinksList = createElement({ tag: 'ul', className: 'mobile__links-list' });
+export const mobileLinkHome = createElement({
+  tag: 'li',
+  className: 'mobile__list-item home-link active',
+  textContent: 'Home',
+});
+export const mobileLinkCatalog = createElement({
+  tag: 'li',
+  className: 'mobile__list-item catalog-link',
+  textContent: 'Catalog',
+});
+export const mobileLinkBlogs = createElement({
+  tag: 'li',
+  className: 'mobile__list-item blogs-link',
+  textContent: 'Blogs',
+});
+export const mobileSearchLink = createElement({
+  tag: 'li',
+  className: 'mobile__list-item mobile__search',
+  textContent: 'Search',
+});
+export const mobileBasketLink = createElement({
+  tag: 'li',
+  className: 'mobile__list-item mobile__basket',
+  textContent: 'Basket',
+});
+
+export const mobileLoginButton = createElement({
+  tag: 'li',
+  className: 'mobile__list-item mobile__login',
+  textContent: 'Login',
+});
+
+mobileLinksList.append(
+  mobileLinkHome,
+  mobileLinkCatalog,
+  mobileLinkBlogs,
+  mobileSearchLink,
+  mobileBasketLink,
+  mobileLoginButton
+);
+
+export const mobileMenu = createElement({ tag: 'div', className: 'mobile-menu' });
+mobileMenu.append(mobileLinksList);
+
 linkWrap.append(searchLink, basketLink, loginButton);
-header.append(logoLink, headerLinksList, linkWrap);
+header.append(logoLink, headerLinksList, linkWrap, burgerMenu, mobileMenu);
 
 export default header;
--- src/handlers/navigationHandlers.ts
+++ src/handlers/navigationHandlers.ts
@@ -1,11 +1,21 @@
 import { loginLink, registerLink } from '../components/loginHeader/loginHeader';
 import { buttonHome } from '../pages/page404/page404';
 import {
+  basketLink,
+  burgerMenu,
   headerLinkBlogs,
   headerLinkCatalog,
   headerLinkHome,
   loginButton,
   logoLink,
+  mobileBasketLink,
+  mobileLinkBlogs,
+  mobileLinkCatalog,
+  mobileLinkHome,
+  mobileLoginButton,
+  mobileMenu,
+  mobileSearchLink,
+  searchLink,
 } from '../components/header/header';
 import navigate from '../utils/navigate';
 
@@ -33,6 +43,15 @@ headerLinkBlogs.addEventListener('click', (event) => {
   navigate('/blogs');
 });
 
+searchLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  navigate('/');
+});
+basketLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  navigate('/blogs');
+});
+
 loginButton.addEventListener('click', (event) => {
   event.preventDefault();
   navigate('/login');
@@ -46,4 +65,49 @@ loginLink.addEventListener('click', (event) => {
 registerLink.addEventListener('click', (event) => {
   event.preventDefault();
   navigate('/register');
+});
+
+burgerMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 550) {
+    mobileMenu.classList.remove('active');
+    burgerMenu.classList.remove('active');
+  }
+});
+
+mobileLinkHome.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/');
+});
+
+mobileLinkCatalog.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/catalog');
+});
+mobileLinkBlogs.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/blogs');
+});
+mobileSearchLink.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/');
+});
+mobileBasketLink.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/');
+});
+
+mobileLoginButton.addEventListener('click', (event) => {
+  mobileMenu.classList.toggle('active');
+  event.preventDefault();
+  navigate('/login');
 });
