9a64032be365b5f9d9b785d93b8b17b939d40564
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-05-20 20:08:02 +0400

    feat: add links to main page

--- src/pages/mainPage/mainPage.ts
+++ src/pages/mainPage/mainPage.ts
@@ -1,5 +1,39 @@
 import createElement from '../../helpers/createElement';
+import './_mainPage.scss';
 
 const mainPage = createElement({ tag: 'section', className: 'main', textContent: 'MAIN PAGE' });
+const linksList = createElement({ tag: 'div', className: 'links-list' });
+const mainLink = createElement({
+  tag: 'a',
+  href: '',
+  className: 'links-list__link',
+  textContent: 'Main Page',
+});
+const loginLink = createElement({
+  tag: 'a',
+  href: '',
+  className: 'links-list__link',
+  textContent: 'Log in',
+});
+const registerLink = createElement({
+  tag: 'a',
+  href: '',
+  className: 'links-list__link',
+  textContent: 'Register',
+});
+const catalogLink = createElement({
+  tag: 'a',
+  href: '',
+  className: 'links-list__link',
+  textContent: 'Catalog',
+});
+const blogsLink = createElement({
+  tag: 'a',
+  href: '',
+  className: 'links-list__link',
+  textContent: 'Blogs',
+});
+mainPage.append(linksList);
+linksList.append(mainLink, loginLink, registerLink, catalogLink, blogsLink);
 
 export default mainPage;
