a1fa78aabac923c6236a5d388b088bb7a40878f1
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-05-26 18:22:43 +0400

    feat: add catalog card component

--- src/pages/catalogPage/catalogPage.ts
+++ src/pages/catalogPage/catalogPage.ts
@@ -1,9 +1,20 @@
 import createElement from '../../helpers/createElement';
+import createCatalogCard from '../../components/catalogCard/catalogCard';
 
 const catalogPage = createElement({
   tag: 'section',
   className: 'catalog',
   textContent: 'CATALOG PAGE',
 });
+
+const card = createCatalogCard(
+  'https://www.ikea.com/gb/en/images/products/clusia-potted-plant-clusia__0653977_pe708206_s5.jpg?f=xl',
+  '#',
+  'Dypsis lutescens',
+  '12',
+  '14',
+  '5'
+);
+catalogPage.append(card);
 
 export default catalogPage;
