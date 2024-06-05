3f1f87933318d442b6f9f68b437ec42c96d9c06c
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-06-05 23:47:53 +0400

    refactor: refactor search gunction

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,8 +1,6 @@
 import createElement from '../../helpers/createElement';
 import { basket, login, logoSvg, register, search, userSvg } from '../../assets/icons/index';
-// import generateCatalog from '../catalog/catalog';
-import generateCatalogPage from '../../pages/catalogPage/catalogPage';
-import { productParams } from '../catalog/catalog';
+import handleSearch from '../../utils/handleSearch';
 
 const header = createElement({ tag: 'header', className: 'header container' });
 export const logoLink = createElement({ tag: 'a', className: 'logo__link' });
@@ -44,20 +42,8 @@ const searchInput = createElement({
   type: 'search',
 });
 
-searchLinkBtn.addEventListener('click', async () => {
-  if (searchInput.value !== '') {
-    const main = document.querySelector('main');
-    main!.innerHTML = '';
-    productParams.searchText = searchInput.value;
-    productParams.category = undefined;
-    productParams.filterPrice = undefined;
-    productParams.sort = undefined;
-
-    const catalog = await generateCatalogPage({ searchText: searchInput.value });
-    main?.append(catalog);
-  } else {
-    searchInput.focus();
-  }
+searchLinkBtn.addEventListener('click', () => {
+  handleSearch(searchInput.value, searchInput);
 });
 
 searchLinkBtn.innerHTML = search;
@@ -150,22 +136,11 @@ const mobileSearchBtn = createElement({
 });
 mobileSearchBtn.innerHTML = search;
 
-mobileSearchBtn.addEventListener('click', async () => {
-  if (mobileSearchInput.value !== '') {
-    const main = document.querySelector('main');
-    main!.innerHTML = '';
-    productParams.searchText = mobileSearchInput.value;
-    productParams.category = undefined;
-    productParams.filterPrice = undefined;
-    productParams.sort = undefined;
-
-    const catalog = await generateCatalogPage({ searchText: mobileSearchInput.value });
-    main?.append(catalog);
+mobileSearchBtn.addEventListener('click', () => {
+  handleSearch(mobileSearchInput.value, mobileSearchInput).then(() => {
     mobileMenu.classList.remove('active');
     burgerMenu.classList.remove('active');
-  } else {
-    mobileSearchInput.focus();
-  }
+  });
 });
 
 mobileSearchLink.append(mobileSearchInput, mobileSearchBtn);
