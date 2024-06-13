f8e590e648061501636828ae663427ec785eb5fe
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-06-13 14:14:34 +0400

    pagination btns, styling and routing in progress
    feat: add pagination btns
    feat: styling for pagination
    feat: pagination routing wip

---
+++ src/components/catalog/pagination.ts
@@ -0,0 +1,24 @@
+import createElement from '../../helpers/createElement';
+import navigate from '../../utils/navigate';
+
+export default function generatePagination(pagLength: number) {
+  const pagination = createElement({ tag: 'div', className: 'pagination' });
+  for (let i = 1; i <= pagLength; i += 1) {
+    const pagItem = createElement({
+      tag: 'button',
+      className: 'pagination__item',
+      textContent: i.toString(),
+      id: `pag-item-${i}`,
+    });
+    pagItem.addEventListener('click', () => {
+      const currLocation = window.location.href;
+      const urlPart = currLocation.replace(/.*(\/catalog.*)/, '$1');
+      const cleanedUrl = urlPart.replace(/&page=\d+/, '');
+      const newAddress = `${cleanedUrl}&page=${i} `;
+      navigate(newAddress);
+    });
+
+    pagination.append(pagItem);
+  }
+  return pagination;
+}
