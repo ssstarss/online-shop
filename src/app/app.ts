88f28aaa89151ea927d7fc2e51731d2eba492f3a
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-06-14 14:53:44 +0400

    feat: always display cart button on catalog card
    feat: always display cart button on catalog card
    fix: update active pagination btn on load
    refactor: refactor app fuction and items in cart on load

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,9 @@ import footerContainer from '../components/footer/footer';
 import header from '../components/header/header';
 import generatePreloader from '../components/loader/loader';
 import mainContainer from '../components/mainContainer/mainContainer';
+import getCart from '../utils/getCart';
 import initRouting from '../utils/router2';
+import updateCartInHeader from '../utils/updateCartInHeader';
 import { connectionByFetch } from './connectionByFetch';
 
 const connectionByFetchInit = connectionByFetch;
@@ -10,10 +12,21 @@ const connectionByFetchInit = connectionByFetch;
 const App = async () => {
   const preloader = generatePreloader();
   document.body.append(preloader);
-  await connectionByFetchInit.init();
-  document.body.append(header, mainContainer, footerContainer);
-  initRouting();
-  preloader.classList.add('hidden');
+
+  try {
+    await connectionByFetchInit.init();
+
+    const [cartResponse] = await Promise.all([getCart()]);
+
+    const totalItemsInCart = cartResponse.totalLineItemQuantity;
+    updateCartInHeader(totalItemsInCart);
+  } catch (error) {
+    console.error('Error during initialization:', error);
+  } finally {
+    document.body.append(header, mainContainer, footerContainer);
+    initRouting();
+    preloader.classList.add('hidden');
+  }
 };
 
 document.addEventListener('DOMContentLoaded', App);
---
+++ src/utils/updateCartInHeader.ts
@@ -0,0 +1,22 @@
+export default function updateCartInHeader(productsAmountInCart: number) {
+  if (productsAmountInCart) {
+    const svg = document.querySelector('.header__basket svg') as SVGSVGElement;
+    const productCounterExisting = document.getElementById('cart-counter');
+    if (productCounterExisting) {
+      productCounterExisting.textContent = productsAmountInCart.toString();
+    } else {
+      const productCounter = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+      productCounter.setAttribute('x', (23).toString());
+      productCounter.setAttribute('y', (9).toString());
+      productCounter.setAttribute('font-family', 'CeraPro');
+      productCounter.setAttribute('font-size', '11');
+      productCounter.setAttribute('fill', 'white');
+      productCounter.setAttribute('text-anchor', 'middle');
+      productCounter.setAttribute('dominant-baseline', 'middle');
+
+      productCounter.id = 'cart-counter';
+      productCounter.textContent = productsAmountInCart.toString();
+      svg.append(productCounter);
+    }
+  }
+}
